7d9b6827dc353ea1ef2c1149414358d7c5830f7f
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2022-01-19 23:14:31 +0300

    edit header style

--- src/pages/PageCovidSummary.vue
+++ src/pages/PageCovidSummary.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-header elevated class="bg-dark row justify-center">
+  <q-header elevated class="bg-dark row justify-start">
+    <q-toolbar-title class="col-4 row justify-center items-center text-weight-bold">Covid-19 Statistics</q-toolbar-title>
     <CountrySearchBar />
   </q-header>
 
